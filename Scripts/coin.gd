58905a16c12c006edf48f719a2d2bab7275ff50c
AvareGameStudio <avaregamestudio@gmail.com>
2026-05-12 15:06:20 +0300

    Persist best score and polish coin pickup

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -2,14 +2,26 @@ extends Area2D
 
 @export var score_value: int = 1
 
+var is_collected: bool = false
+
 func _ready() -> void:
 	# Sinyali kodla bağlayarak editör hatalarını devre dışı bırakıyoruz
 	body_entered.connect(_on_body_entered)
-	
+		
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("players") and not GameManager.is_game_over:
+	if body.is_in_group("players") and not GameManager.is_game_over and not is_collected:
+		is_collected = true
 		GameManager.add_score(score_value)
-		call_deferred("queue_free")
+		$CollisionShape2D.set_deferred("disabled", true)
+		play_collect_feedback()
 
 func free_coin() -> void:
 	queue_free()
+
+func play_collect_feedback() -> void:
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "position:y", position.y - 24.0, 0.18)
+	tween.tween_property(self, "scale", scale * 1.25, 0.12)
+	tween.tween_property(self, "modulate:a", 0.0, 0.18)
+	tween.chain().tween_callback(queue_free)
--- Scripts/gameManager.gd
+++ Scripts/gameManager.gd
@@ -3,11 +3,14 @@ extends Node
 signal score_changed(score: int, high_score: int)
 signal game_over(score: int, high_score: int)
 
+const SAVE_PATH = "user://save.cfg"
+
 var score: int = 0
 var high_score: int = 0
 var is_game_over: bool = false
 
 func _ready() -> void:
+	load_progress()
 	reset_score()
 
 func add_score(amount: int) -> void:
@@ -17,6 +20,7 @@ func add_score(amount: int) -> void:
 	score += amount
 	if score > high_score:
 		high_score = score
+		save_progress()
 	score_changed.emit(score, high_score)
 
 func reset_score() -> void:
@@ -34,3 +38,16 @@ func end_run() -> void:
 func restart_run() -> void:
 	reset_score()
 	get_tree().reload_current_scene.call_deferred()
+
+func load_progress() -> void:
+	var save_file = ConfigFile.new()
+	var error = save_file.load(SAVE_PATH)
+	if error != OK:
+		return
+
+	high_score = int(save_file.get_value("progress", "high_score", 0))
+
+func save_progress() -> void:
+	var save_file = ConfigFile.new()
+	save_file.set_value("progress", "high_score", high_score)
+	save_file.save(SAVE_PATH)
